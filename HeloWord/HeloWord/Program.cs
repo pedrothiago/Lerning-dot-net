11beab9eb502faad07c7151bea3f44dadacab15c
Pedro Thiago Alcântara <ptralcantara@gmail.com>
2017-12-15 11:35:46 -0200

    helo word
    helo word

---
+++ HeloWord/HeloWord/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HeloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
